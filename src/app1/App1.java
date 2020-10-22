87c92ce6634770994c68cb72b67b769ab620ad88
AMY <AMY@DESKTOP-HSJRQSM>
2020-10-22 06:39:25 -0500

    Actualización 22/10/2020

---
+++ src/app1/App1.java
@@ -0,0 +1,16 @@
+package app1;
+
+public class App1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a, b, suma;
+        a =2;
+        b= 3;
+        
+    }
+    
+}
